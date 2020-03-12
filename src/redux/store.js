442f0c9d3a19eaec2e76859edd81e1599b50e183
UsamaAslam <uaslam345@gmail.com>
2020-03-12 17:49:41 +0500

    initial setup with Redux

---
+++ src/redux/store.js
@@ -0,0 +1,17 @@
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+import rootReducer from "./rootReducer";
+
+const persistConfig = {
+  key: "root",
+  storage
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+let store = createStore(persistedReducer, applyMiddleware(thunk));
+let persistor = persistStore(store);
+
+export { store, persistor };
