1652b69ef0dd1baba544933ed9335b7a2b69d690
UsamaAslam <uaslam345@gmail.com>
2020-03-24 15:32:36 +0500

    refactoring

--- src/views/Login/LoginCard.js
+++ src/views/Login/LoginCard.js
@@ -1,4 +1,4 @@
-import React, { Component, useState, useEffect } from "react";
+import React from "react";
 import { useHistory } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 
