8125df54433437506f47d32050e6a24e83dda765
UsamaAslam <uaslam345@gmail.com>
2020-03-26 00:03:01 +0500

    keys changing

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,14 +1,14 @@
 import * as firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDDTesYc6zp_GQMJpL1UajCRY75YiYmC8U",
+  apiKey: "key",
   authDomain: "task-login-pk.firebaseapp.com",
   databaseURL: "https://task-login-pk.firebaseio.com",
   projectId: "task-login-pk",
   storageBucket: "task-login-pk.appspot.com",
   messagingSenderId: "477711390020",
-  appId: "1:477711390020:web:0066cfbffb06b5a946b311",
-  measurementId: "G-E2NFJTFLQD"
+  appId: "key",
+  measurementId: "key"
 };
 
 firebase.initializeApp(firebaseConfig);
