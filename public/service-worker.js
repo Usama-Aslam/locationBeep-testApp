cb728b57fc0eddeba1f65cc8c14080bdf119943b
UsamaAslam <uaslam345@gmail.com>
2020-03-25 22:54:46 +0500

    path error solved

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,7 +2,7 @@
 var doCache = true;
 
 // Name our cache
-var CACHE_NAME = "locationBeep-v1";
+var CACHE_NAME = "locationBeep-v2";
 
 // Delete old caches that are not our current one!
 self.addEventListener("activate", event => {
